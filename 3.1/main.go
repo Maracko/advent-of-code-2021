01f00011ca860dbfa865090f84dfdbd9f0bee065
Mario Petričko <petricko.mario@gmail.com>
2021-12-03 21:03:17 +0100

    fix formatting

--- 3.1/main.go
+++ 3.1/main.go
@@ -12,17 +12,18 @@ func main() {
 	defer f.Close()
 
 	counter := make(map[int]map[string]int,12)
-
 	for i := 0; i < 12; i++ {
 		counter[i] = make(map[string]int,2)
 	}
+
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan(){
 		line := scanner.Text()
 		for i,char := range line {
 			counter[i][string(char)] += 1
 		}
-}
+	}
+
 	gamma := ""
 	for i:=0;i< len(counter);i++ {
 		if counter[i]["1"] > counter[i]["0"] {
@@ -41,12 +42,8 @@ func main() {
 		}
 	}
 	
-	fmt.Printf("bin gamma: %s\nbin epsilon: %s\n",gamma,epsilon)
-
 	decGamma, _ := strconv.ParseInt(gamma,2,64)
 	decEpsilon, _ := strconv.ParseInt(epsilon,2,64)
-
-	fmt.Printf("dec gamma: %d\ndec epsilon: %d\n",decGamma,decEpsilon)
 
 	fmt.Println("Part 1 solution:", decGamma * decEpsilon)
 }
