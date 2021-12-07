c5d2f687033457eedc54cc1d9a12bb70d84b932a
Mario Petričko <petricko.mario@gmail.com>
2021-12-07 22:16:24 +0100

    working day 5 part 2, need to fix this!!!!

--- 5/main.go
+++ 5/main.go
@@ -22,11 +22,12 @@ func main() {
 
 	var coords []coordinate
 	for _, val := range data {
-		coords = append(coords, getCoordinates(val))
+		c := getCoordinates(val)
+		coords = append(coords, c)
 	}
 
-	cMat := create2DMatrix(10)
-	cMat2 := create2DMatrix(10)
+	cMat := create2DMatrix(1000)
+	cMat2 := create2DMatrix(1000)
 
 	addLinesToMatrix(cMat, coords, false)
 	addLinesToMatrix(cMat2, coords, true)
@@ -43,6 +44,7 @@ func getCoordinates(line string) coordinate {
 	var c coordinate
 	fmt.Sscanf(line, "%d,%d -> %d,%d\n", &c.x1, &c.y1, &c.x2, &c.y2)
 	return c
+
 }
 
 func create2DMatrix(length int) Matrix2D {
@@ -53,61 +55,103 @@ func create2DMatrix(length int) Matrix2D {
 	return m
 }
 
-func getSortedCoordValue(coord coordinate, axis string) (low int, high int) {
-	switch axis {
-	case "x":
-		if coord.x1 < coord.x2 {
-			low = coord.x1
-			high = coord.x2
-		} else {
-			low = coord.x2
-			high = coord.x1
-		}
-	case "y":
-		if coord.y1 < coord.y2 {
-			low = coord.y1
-			high = coord.y2
-		} else {
-			low = coord.y2
-			high = coord.y1
-		}
+func sortCoordinatesByValue(c *coordinate) {
+
+	if c.x2 < c.x1 {
+		c.x1, c.x2 = c.x2, c.x1
 	}
-	return
+	if c.y2 < c.y1 {
+		c.y1, c.y2 = c.y2, c.y1
+	}
 }
+
+// func getSortedCoordValue(coord coordinate, axis string) (low int, high int) {
+// 	switch axis {
+// 	case "x":
+// 		if coord.x1 < coord.x2 {
+// 			low = coord.x1
+// 			high = coord.x2
+// 		} else {
+// 			low = coord.x2
+// 			high = coord.x1
+// 		}
+// 	case "y":
+// 		if coord.y1 < coord.y2 {
+// 			low = coord.y1
+// 			high = coord.y2
+// 		} else {
+// 			low = coord.y2
+// 			high = coord.y1
+// 		}
+// 	}
+// 	return
+// }
 
 func addLinesToMatrix(mat Matrix2D, coords []coordinate, addDiagonal bool) {
 	for _, c := range coords {
-		lowX, highX := getSortedCoordValue(c, "x")
-		lowY, highY := getSortedCoordValue(c, "y")
-
 		if (c.x1 == c.x2) || (c.y1 == c.y2) {
+			sortCoordinatesByValue(&c)
 			sameCol := c.x1 == c.x2
 			sameRow := c.y1 == c.y2
 			switch {
 			case sameCol:
 				column := c.x1
-				for row := lowY; row <= highY; row++ {
+				for row := c.y1; row <= c.y2; row++ {
 					mat[row][column]++
 				}
 			case sameRow:
 				row := c.y1
-				for column := lowX; column <= highX; column++ {
+				for column := c.x1; column <= c.x2; column++ {
 					mat[row][column]++
 				}
 			}
 		} else if addDiagonal {
-			for col := lowX; col <= highX; col++ {
-				for row := lowY; row <= highY; row++ {
-					if col == row {
-						if lowX <= lowY {
-							mat[row][col]++
-						} else {
-							mat[row][col]++
+			i := 0
+			j := 0
+			if c.x1 < c.x2 {
+				for col := c.x1; col <= c.x2; col++ {
+					i++
+					if c.y1 < c.y2 {
+						for row := c.y1; row <= c.y2; row++ {
+							j++
+							if i == j {
+								mat[row][col]++
+							}
+						}
+
+					} else {
+						for row := c.y1; row >= c.y2; row-- {
+							j++
+							if i == j {
+								mat[row][col]++
+							}
 						}
 					}
+					j = 0
+
+				}
+			} else {
+				for col := c.x1; col >= c.x2; col-- {
+					i++
+					if c.y1 < c.y2 {
+						for row := c.y1; row <= c.y2; row++ {
+							j++
+							if i == j {
+								mat[row][col]++
+							}
+						}
+
+					} else {
+						for row := c.y1; row >= c.y2; row-- {
+							j++
+							if i == j {
+								mat[row][col]++
+							}
+						}
+					}
+					j = 0
 				}
 			}
-
 		}
 	}
 }
